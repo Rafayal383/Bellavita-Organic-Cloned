55c344c0223f5e2c5fed4af04e1b7059891ecc42
Shalini 1908 <989163962g@gmail.com>
2022-09-29 23:39:01 +0530

    added skincare

---
+++ skincare/js_files/box.js
@@ -0,0 +1,43 @@
+export const Box = ({ img1, img, img2, pname, offprice, price, Ratings }) => {
+  return ` <div>
+
+  <div id="best" > 
+      <img src="${img1}"
+          alt="">
+          
+  </div>
+
+  <div id="off">35% OFF</div>
+
+  <div id="image">
+
+  <img id="product_img" src=${img} 
+      alt="">
+   <div class="inner">
+   <img src=${img2} /></div>
+     
+
+  </div>
+  <i id="wishlist" class="fa-regular fa-heart"></i>
+
+  <p id="bigp">${pname}</p><br>
+  <p id="smallp"></p>
+
+  <div id="price">
+      <p><i class="fa-solid fa-indian-rupee-sign"></i>${offprice}</p>
+      <p><i class="fa-solid fa-indian-rupee-sign"></i>${price}</p>
+
+  </div>
+
+  <div id="star">
+      <i class="fa-sharp fa-solid fa"><i class="fa-solid fa-star">${Ratings}</i></i>
+
+  </div>
+
+  <button id="button">Add to Cart</button>
+
+</div>
+`;
+};
+
+
---
+++ skincare/js_files/skincare_5.js
@@ -0,0 +1,36 @@
+let FaceserumData= [
+    {
+      img1: "https://cdn.shopify.com/s/files/1/0054/6665/2718/files/Best-sellers-tag.png?v=10809169506792369733",
+      img: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/GLOWNER-01_360x.jpg?v=1652168666",
+      img2:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/GLOWNER-02_0cf682c0-a164-4bdf-b240-8795532c4bf8_360x.jpg?v=1652168666",
+      pname: "Glowner (Face Toner), 100ml",
+      offprice: "174.00",
+      price: "249.00",
+      Ratings: "4.5",
+    },
+   
+    {
+        img1: "https://cdn.shopify.com/s/files/1/0054/6665/2718/files/Best-sellers-tag.png?v=10809169506792369733",
+        img: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/KumkumadiTheUltimateRadianceCombo-01_ad46d098-1eb7-4932-819e-af6f5c19ce5f_360x.jpg?v=1652276671",
+        img2:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/KumkumadiTheUltimateRadianceCombo-02_360x.jpg?v=1652276685",
+        pname: "The Ultimate Radiance Combo",
+        offprice: "804.00",
+        price: "1,148.00",
+        Ratings: "4.5",
+      },
+   
+      
+
+      {
+        img1: "https://cdn.shopify.com/s/files/1/0054/6665/2718/files/Best-sellers-tag.png?v=10809169506792369733",
+        img: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Kumkumadi-Face-Oil-01_360x.jpg?v=1652341175",
+        img2:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/KumkumadiFaceOil-02_360x.jpg?v=1652341175",
+        pname: "Ayurvedic Kumkumadi Face Oil, 30ml",
+        offprice: "389.00",
+        price: "599.00",
+        Ratings: "4.5",
+      },
+
+]
+
+export default FaceserumData;
