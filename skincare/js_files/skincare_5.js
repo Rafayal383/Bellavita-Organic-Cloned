0d6f4c135ced057cf9b16a98b65ff66ed679c940
Shalini 1908 <989163962g@gmail.com>
2022-09-30 16:20:38 +0530

    added skincare

--- skincare/js_files/skincare_5.js
+++ skincare/js_files/skincare_5.js
@@ -9,27 +9,19 @@ let FaceserumData= [
       Ratings: "4.5",
     },
    
-    {
-        img1: "https://cdn.shopify.com/s/files/1/0054/6665/2718/files/Best-sellers-tag.png?v=10809169506792369733",
-        img: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/KumkumadiTheUltimateRadianceCombo-01_ad46d098-1eb7-4932-819e-af6f5c19ce5f_360x.jpg?v=1652276671",
-        img2:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/KumkumadiTheUltimateRadianceCombo-02_360x.jpg?v=1652276685",
-        pname: "The Ultimate Radiance Combo",
-        offprice: "804.00",
-        price: "1,148.00",
-        Ratings: "4.5",
-      },
    
+    {
+      img1: "https://cdn.shopify.com/s/files/1/0054/6665/2718/files/Best-sellers-tag.png?v=10809169506792369733",
+      img: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/C-GlowFaceSerum-01_1_360x.jpg?v=1651698248",
+      img2:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/C-GlowFaceSerum-06_9d045130-dcb9-4c59-aa41-12ef9cafcdd1_360x.jpg?v=1652182969",
+      pname: "C-Glow Face Wash, 100ml",
+      offprice:"263.00",
+      price: "375.00",
+      Ratings: "4.5",
+    },
       
 
-      {
-        img1: "https://cdn.shopify.com/s/files/1/0054/6665/2718/files/Best-sellers-tag.png?v=10809169506792369733",
-        img: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Kumkumadi-Face-Oil-01_360x.jpg?v=1652341175",
-        img2:"https://cdn.shopify.com/s/files/1/0054/6665/2718/products/KumkumadiFaceOil-02_360x.jpg?v=1652341175",
-        pname: "Ayurvedic Kumkumadi Face Oil, 30ml",
-        offprice: "389.00",
-        price: "599.00",
-        Ratings: "4.5",
-      },
+    
 
 ]
 
