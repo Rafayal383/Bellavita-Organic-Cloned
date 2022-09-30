93ede25d562731165c727056f444fafd23b747b7
Shalini 1908 <989163962g@gmail.com>
2022-09-30 16:41:59 +0530

    added skincare

--- skincare/js_files/Allproduct.js
+++ skincare/js_files/Allproduct.js
@@ -59,3 +59,62 @@ function addTocart() {
 //  productAppend(Pdata)
 
 // }
+
+
+document.querySelector("#SortBy").addEventListener("change", handlePriceSort);
+function handlePriceSort() {
+  let selected = document.querySelector("#SortBy").value;
+  if (selected == "price-ascending") {
+    product_array.sort(function (a, b) {
+      let x = +a.price;
+      let y = +b.price;
+      if (x > y) return 1;
+      if (x < y) return -1;
+      return 0;
+    });
+    productAppend(product_array);
+    console.log("abc");
+  }
+  if (selected == "price-descending") {
+    product_array.sort(function (a, b) {
+      let x = +a.price;
+      let y = +b.price;
+      if (x > y) return -1;
+      if (x < y) return 1;
+      return 0;
+    });
+    productAppend(product_array);
+  }
+  if (selected == "title-descending") {
+    product_array.sort(function (a, b) {
+      let x = a.pname;
+      let y = b.pname;
+      if (x > y) return -1;
+      if (x < y) return 1;
+      return 0;
+    });
+    productAppend(product_array);
+  }
+  if (selected == "title-ascending") {
+    product_array.sort(function (a, b) {
+      let x = a.pname;
+      let y = b.pname;
+      if (x > y) return 1;
+      if (x < y) return -1;
+      return 0;
+    });
+    productAppend(product_array);
+  }
+  if (selected == "selected") {
+    productAppend(product_array);
+  }
+  if (selected == "best-selling") {
+    product_array = product_array.filter(function (el) {
+      return (
+        el.img1 ==
+        "https://cdn.shopify.com/s/files/1/0054/6665/2718/files/Best-sellers-tag.png?v=10809169506792369733"
+      );
+    });
+    productAppend(product_array);
+  }
+}
