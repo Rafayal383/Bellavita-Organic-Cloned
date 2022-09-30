8a97b0e4b68916cd3c610975c60e408321f1807b
SristyVerma <sristyel2016@gmail.com>
2022-09-30 14:03:04 +0530

    cart done

--- Cart/cart.js
+++ Cart/cart.js
@@ -3,25 +3,31 @@ let data = JSON.parse(localStorage.getItem("cart")) || [];
 display(data);
 function display(data) {
   let menu = document.getElementById("content");
-  menu.innerHTML = "";
+  menu.innerHTML = null;
   //   let total = 0;
   //   total_amount.innerHTML = "";
   data.forEach(function (el, i) {
     let div = document.createElement("div");
-    let pname = document.createElement("h3");
+    div.id = "card";
+    let box = document.createElement("div");
+    box.id = "parabox";
+    let pname = document.createElement("p");
     pname.innerText = el.pname;
+    let price = document.createElement("p");
+    price.innerText = "Rs." + el.price;
+
     let image = document.createElement("img");
     image.src = el.img;
-    let price = document.createElement("p");
-    price.innerText = el.price;
+
     let icon = document.createElement("i");
-    icon.className = "fa-light fa-trash-can";
+    icon.className = "fa-regular fa-trash-can";
     icon.id = "remove";
     icon.addEventListener("click", function () {
       remove_product(el, i);
     });
     //     total += +el.price;
-    div.append(image, pname, price, icon);
+    box.append(pname, price);
+    div.append(image, box, icon);
     menu.append(div);
   });
 }
